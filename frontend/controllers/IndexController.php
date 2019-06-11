ec03f71c0bcc572166929bd98e0d9d3a0ecdee0e
zhangzhongxian <zhangzhongxian@sanlischool.com>
2019-06-11 10:37:33 +0800

    first commit

---
+++ frontend/controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangzhongxian
+ * Date: 2019/6/10
+ * Time: 16:54
+ */
+
+namespace frontend\controllers;
+
+
+use yii\web\Controller;
+
+class IndexController extends Controller
+{
+
+    public function actionIndex()
+    {
+    }
+}
